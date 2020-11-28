a60a9366a61ec6662e51e1eeb8744635b4126c6b
ina.ryu <ina.ryu@navercorp.cp>
2020-11-28 20:56:15 +0900

    fix radio button handler

--- src/components/AdButton.jsx
+++ src/components/AdButton.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class AdButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(event) {
+        console.log(event.target.value);
+        this.setState({value: event.target.value})
+    }
+    
     render() {
         return (
             <div>
                 <p>광고동의</p>
-                <div onChange={this.onChangeValue}>
-                    <input type="radio" value="agree" name="ad" /> 동의함
-                    <input type="radio" value="disagree" name="ad" /> 동의안함
+                <div>
+                    <input type="radio" checked={this.state.value === "true"} value={true} name="ad" onChange={this.onChange}/> 동의함
+                    <input type="radio" checked={this.state.value === "false"} value={false} name="ad" onChange={this.onChange}/> 동의안함
                 </div>
             </div>
         );
